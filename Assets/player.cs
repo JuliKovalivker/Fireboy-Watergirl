f71059b31d8a03d7dc6ef8ceed611b47d41d1b3d
JuliKovalivker <101721164+JuliKovalivker@users.noreply.github.com>
2022-05-27 10:52:23 -0300

    Initial commit

---
+++ Assets/player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public int jumpForce = 5;
+    bool hasJump;
+    Rigidbody RB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && hasJump)
+        {
+            RB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            hasJump = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        hasJump = true;
+        Debug.Log("HOLA!");
+    }
+}
+ 
