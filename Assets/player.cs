0802ce718be23bb9c1c4d9734bad336e6ddddad6
JuliKovalivker <101721164+JuliKovalivker@users.noreply.github.com>
2022-06-02 10:50:32 -0300

    cosas

--- Assets/player.cs
+++ Assets/player.cs
@@ -17,17 +17,34 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && hasJump)
+        if (Input.GetKey(KeyCode.Space) && hasJump)
         {
             RB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             hasJump = false;
         }
+        else if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.position += new Vector3(-0.1f, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.transform.position += new Vector3(0, 0, -0.1f);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position += new Vector3(0.1f, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            gameObject.transform.position += new Vector3(0, 0, 0.1f);
+        }
+
+        gameObject.transform.rotation = new Vector3(0, 0, 0);
     }
 
     void OnCollisionEnter(Collision coll)
     {
         hasJump = true;
-        Debug.Log("HOLA!");
     }
 }
  
