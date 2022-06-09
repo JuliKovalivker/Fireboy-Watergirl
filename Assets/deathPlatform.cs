a3deb28ce6f127f0f98f29d0715da731cb148fd2
JuliKovalivker <101721164+JuliKovalivker@users.noreply.github.com>
2022-06-09 10:17:46 -0300

    avance

---
+++ Assets/deathPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathPlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionStay()
+    {
+        Destroy(gameObject, 0.1f);
+    }
+}
---
+++ Assets/puerta.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class puerta : MonoBehaviour
+{
+    public GameObject texto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter()
+    {
+        Debug.Log("Ganaste!");
+        texto.SetActive(true);
+
+    }
+}
