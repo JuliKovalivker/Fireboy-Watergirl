ece3d118be4a7da026310eb51af05c3442291cb6
JuliKovalivker <101721164+JuliKovalivker@users.noreply.github.com>
2022-06-16 14:11:57 -0300

    no se algo

---
+++ Assets/Scripts/monedas.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class monedas : MonoBehaviour
+{
+    public Text UIText;
+    int contador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contador = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UIText = GameObject.FindGameObjectWithTag("Text").GetComponent<Text>();
+        UIText.text = gameObject.GetComponent<Text>().text;
+    }
+
+    void OnCollisionEnter()
+    {
+        Debug.Log("hola");
+        contador++;
+        Debug.Log(contador);
+        UIText.text = contador + " monedas ganadas";
+                    
+    }
+}
